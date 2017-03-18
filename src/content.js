94f414ae75b16ad5ede608fed28ade9417f40e25
1forh <1forhlol@gmail.com>
2017-03-18 13:02:48 -0400

    switches icons and colors of grid

--- src/content.js
+++ src/content.js
@@ -3,41 +3,41 @@ function showGrid() {
 		<div id="forh-bs-grid" style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 1000000; pointer-events: none;">
 			<div class="container" style="height: 100%;">
 				<div class="row" style="height: 100%;">
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
-					<div class="col-xs-1" style="height: 100%; background-color: rgba(111, 84, 153, 0.2); position: relative">
-						<div style="background-color: rgba(111, 84, 153, 0.2); height: 100%;"></div>
+					<div class="col-xs-1" style="height: 100%; background-color: rgba(76, 199, 142, 0.2); position: relative">
+						<div style="background-color: rgba(76, 199, 142, 0.2); height: 100%;"></div>
 					</div>
 				</div>
 			</div>
